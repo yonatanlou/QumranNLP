5a71f9f8e9a328cdb0be0e1a65b0ec375fd35245
Yonatan Lourie <yonatan.lourie@forter.com>
2023-10-04 20:05:14 -0400

    lint + better infra

--- clusters.py
+++ clusters.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 from sknetwork.hierarchy import dasgupta_score
 import sklearn.cluster as sk
 
+from logger import get_logger
+
+logger = get_logger(__name__)
+
 
 def get_linkage_matrix(model):
     # create the counts of samples under each node
@@ -31,20 +35,27 @@ def plot_dendrogram(model, sample_names, adj_mat, title, fig_name, **kwargs):
     score = dasgupta_score(adj_mat, linkage_matrix)
     # Plot the corresponding dendrogram
     plt.figure(figsize=(12, 9), dpi=120)
-    dendrogram(linkage_matrix, **kwargs, leaf_label_func=lambda x: sample_names[x], orientation='right', )
+    dendrogram(
+        linkage_matrix,
+        **kwargs,
+        leaf_label_func=lambda x: sample_names[x],
+        orientation="right",
+    )
 
     plt.title(title, fontsize=16)
-    plt.ylabel('Column:Line')
+    plt.ylabel("Column:Line")
     plt.tick_params(
-        axis='x',  # changes apply to the x-axis
-        which='both',  # both major and minor ticks are affected
+        axis="x",  # changes apply to the x-axis
+        which="both",  # both major and minor ticks are affected
         bottom=False,  # ticks along the bottom edge are off
         top=False,  # ticks along the top edge are off
-        labelbottom=False)
+        labelbottom=False,
+    )
     plt.tick_params(
-        axis='y',  # changes apply to the x-axis
-        which='both',  # both major and minor ticks are affected,
-        labelsize=12)
+        axis="y",  # changes apply to the x-axis
+        which="both",  # both major and minor ticks are affected,
+        labelsize=12,
+    )
 
     plt.tight_layout()
     plt.savefig(fig_name)
@@ -52,24 +63,48 @@ def plot_dendrogram(model, sample_names, adj_mat, title, fig_name, **kwargs):
     return score
 
 
-def get_clusters_scores(features, sample_names, linkage_criterion, file_name, title):
-    model = sk.AgglomerativeClustering(distance_threshold=0, n_clusters=None, linkage=linkage_criterion)
+def get_clusters_scores(
+    features,
+    sample_names,
+    linkage_criterion,
+    path,
+    book_scores,
+    method_name,
+    word_per_samples,
+):
+    file_name = f"{path}/{method_name}_{word_per_samples}_samples_{linkage_criterion}"
+    title = f"{method_name}, {word_per_samples} samples, Linkage Criterion: {linkage_criterion}"
+    model = sk.AgglomerativeClustering(
+        distance_threshold=0, n_clusters=None, linkage=linkage_criterion
+    )
     model.fit_predict(features)
     adjacency_matrix = np.zeros((len(sample_names), len(sample_names)))
     for i in range(0, adjacency_matrix.shape[0] - 1):
         adjacency_matrix[i, i + 1] = 1
         adjacency_matrix[i + 1, i] = 1
 
-    score = plot_dendrogram(model, sample_names, adjacency_matrix, title, file_name) #TODO Proceed from here
-    print(f"clusters scores: {score}")
+    score = plot_dendrogram(
+        model, sample_names, adjacency_matrix, title, file_name
+    )  # TODO Proceed from here
 
     random_scores = []
     for i in range(5000):
         indexes = np.arange(len(features))
         np.random.shuffle(indexes)
-        model = sk.AgglomerativeClustering(distance_threshold=0, n_clusters=None, linkage=linkage_criterion)
+        model = sk.AgglomerativeClustering(
+            distance_threshold=0, n_clusters=None, linkage=linkage_criterion
+        )
         model.fit_predict(features[indexes])
         linkage_matrix = get_linkage_matrix(model)
         random_scores.append(dasgupta_score(adjacency_matrix, linkage_matrix))
-    print(f"Random {linkage_criterion}: mean: {np.mean(random_scores)}, std: {np.std(random_scores)}")
-    return score, np.mean(random_scores), np.std(random_scores)
+    print(
+        f"{method_name}: clusters scores: {score:.4f},Random: [mean: {np.mean(random_scores):.4f}, std: {np.std(random_scores):.4f}]"
+    )
+    book_scores.update(
+        {
+            f"{method_name}_feature_clusters_score": score,
+            f"{method_name}_feature_random_score_mean": np.mean(random_scores),
+            f"{method_name}_feature_random_score_std": np.std(random_scores),
+        }
+    )
+    return book_scores
