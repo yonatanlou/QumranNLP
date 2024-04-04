57f888b86817281f87ad47e37cf4457025bf4168
Yonatan Lourie <yonatanlou@gmail.com>
2024-04-04 12:32:24 +0300

    option for log file

--- logger.py
+++ logger.py
@@ -1,21 +1,28 @@
 import logging
 
 
-def get_logger(file_name):
+def get_logger(file_name, log_file_path=None):
     logger = logging.getLogger(file_name)
     logger.setLevel(logging.INFO)
 
-    # define handler and formatter
-    handler = logging.StreamHandler()
-    # formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+    # Create handlers: one for the console, one for the file
+    console_handler = logging.StreamHandler()
+
+    # Define a formatter
     formatter = logging.Formatter(
         "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s",
         "%m-%d %H:%M:%S",
     )
 
-    # add formatter to handler
-    handler.setFormatter(formatter)
+    # Add the formatter to the handlers
+    console_handler.setFormatter(formatter)
 
-    # add handler to logger
-    logger.addHandler(handler)
+    # Add handlers to the logger
+    logger.addHandler(console_handler)
+
+    if log_file_path:
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
     return logger
