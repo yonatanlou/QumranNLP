2c829122ad7216883a60e3a33aac2c07eee9dd88
Yonatan Lourie <yonatanlou@gmail.com>
2024-07-20 14:26:33 +0300

    baselines

--- src/baselines/embeddings.py
+++ src/baselines/embeddings.py
@@ -1,46 +1,66 @@
 import os
 import pickle
+import pandas as pd
 
 from notebooks.constants import BERT_MODELS
 from notebooks.features import vectorize_text
 
 
-def load_vectorizers(path):
-    if os.path.exists(path):
-        with open(path, "rb") as f:
-            processed_vectorizers = pickle.load(f)
-            print(f"Loaded the embeddings: {list(processed_vectorizers.keys())}")
-    else:
-        processed_vectorizers = {}
-    return processed_vectorizers
+class VectorizerProcessor:
+    """
+    A class to load, process, and save text vectorizers.
 
+    Attributes:
+        df (pd.DataFrame): DataFrame containing the text data.
+        path (str): Path to save/load the vectorizers.
+        vectorizers (list): List of vectorizer types to be processed.
+        reprocess_vectorizers (list): List of vectorizers to be reprocessed.
+        processed_vectorizers (dict): Dictionary storing processed vectorizers.
+    """
 
-def save_vectorizers(path, vectorizers):
-    with open(path, "wb") as f:
-        pickle.dump(vectorizers, f)
+    def __init__(
+        self, df: pd.DataFrame, path: str, vectorizers: list, reprocess_vectorizers=None
+    ):
+        if reprocess_vectorizers is None:
+            reprocess_vectorizers = []
+        self.df = df
+        self.path = path
+        self.vectorizers = vectorizers
+        self.reprocess_vectorizers = reprocess_vectorizers
+        self.processed_vectorizers = self.load_vectorizers()
+
+    def load_vectorizers(self):
+        if os.path.exists(self.path):
+            with open(self.path, "rb") as f:
+                processed_vectorizers = pickle.load(f)
+                print(f"Loaded the embeddings: {list(processed_vectorizers.keys())}")
+        else:
+            processed_vectorizers = {}
+        return processed_vectorizers
+
+    def save_vectorizers(self):
+        with open(self.path, "wb") as f:
+            pickle.dump(self.processed_vectorizers, f)
+
+    def process_vectorizer(self, vectorizer_type, reprocess=False):
+        if vectorizer_type in self.processed_vectorizers and not reprocess:
+            return
+        else:
+            print(f"Processing {vectorizer_type}...")
+            X = vectorize_text(self.df, "text", vectorizer_type)
+            self.processed_vectorizers[vectorizer_type] = X
+
+    def load_or_generate_embeddings(self):
+        reprocess = False
+        for vectorizer_type in self.vectorizers:
+            if vectorizer_type in self.reprocess_vectorizers:
+                reprocess = True
+            self.process_vectorizer(vectorizer_type, reprocess=reprocess)
+
+        self.save_vectorizers()
+        print(f"Loaded the following embeddings {self.processed_vectorizers.keys()}")
+        return self.processed_vectorizers
 
 
 def get_vectorizer_types():
     return BERT_MODELS + ["tfidf", "trigram", "starr"]
-
-
-def process_vectorizer(df, vectorizer_type, processed_vectorizers):
-    if vectorizer_type in processed_vectorizers:
-        return processed_vectorizers
-    else:
-        X = vectorize_text(df, "text", vectorizer_type)
-        processed_vectorizers[vectorizer_type] = X
-        return processed_vectorizers
-
-
-def load_or_genereate_embeddings(df, path, vectorizers):
-    processed_vectorizers = load_vectorizers(path)
-
-    for vectorizer_type in vectorizers:
-        processed_vectorizers = process_vectorizer(
-            df, vectorizer_type, processed_vectorizers
-        )
-
-    save_vectorizers(path, processed_vectorizers)
-    print(f"loaded the following embeddings {processed_vectorizers.keys()}")
-    return processed_vectorizers
