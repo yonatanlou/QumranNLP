f2cc32addfd463265754523ef480185b99f72409
Yonatan Lourie <yonatanlou@gmail.com>
2024-09-18 09:06:28 +0300

    --

--- src/baselines/main.py
+++ src/baselines/main.py
@@ -7,7 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVC
 
-from config import BASE_DIR
+from config import BASE_DIR, DATA_PATH
 from src.baselines.utils import create_adjacency_matrix, set_seed_globally
 from src.baselines.create_datasets import QumranDataset, save_dataset_for_finetuning
 from src.baselines.embeddings import get_vectorizer_types, VectorizerProcessor
@@ -33,26 +33,26 @@ def make_baselines_results(
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
 
-    df = pd.read_csv(data_path)
+    df_origin = pd.read_csv(data_path)
 
     vectorizers = get_vectorizer_types()
-    processor = VectorizerProcessor(df, processed_vectorizers_path, vectorizers)
+    processor = VectorizerProcessor(df_origin, processed_vectorizers_path, vectorizers)
     processed_vectorizers = processor.load_or_generate_embeddings()
     set_seed_globally()
     adjacency_matrix_all = create_adjacency_matrix(
-        df,
+        df_origin,
         context_similiarity_window=3,
         composition_level=False,
     )
 
     dataset_composition = QumranDataset(
-        df, "composition", train_frac, val_frac, processed_vectorizers
+        df_origin, "composition", train_frac, val_frac, processed_vectorizers
     )
     dataset_scroll = QumranDataset(
-        df, "book", train_frac, val_frac, processed_vectorizers
+        df_origin, "book", train_frac, val_frac, processed_vectorizers
     )
     dataset_sectarian = QumranDataset(
-        df, "section", train_frac, val_frac, processed_vectorizers
+        df_origin, "section", train_frac, val_frac, processed_vectorizers
     )
     datasets = {
         "dataset_composition": dataset_composition,
@@ -75,12 +75,11 @@ def make_baselines_results(
 
 
 if __name__ == "__main__":
-    data_path = f"{BASE_DIR}/data/processed_data/filtered_df_CHUNK_SIZE=100_MAX_OVERLAP=15_PRE_PROCESSING_TASKS=[]_2024_02_09.csv"
     results_dir = f"{BASE_DIR}/experiments/baselines"
     processed_vectorizers_path = f"{results_dir}/processed_vectorizers.pkl"
 
     make_baselines_results(
-        data_path,
+        DATA_PATH,
         processed_vectorizers_path,
         results_dir,
         train_frac=0.7,
