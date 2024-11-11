390e7a23d02351d43c62531914466ab1ffbaabac
Yonatan Lourie <yonatanlou@gmail.com>
2024-11-11 21:01:59 +0200

    add results

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,10 @@ BERT_MODELS = [
     "dicta-il/BEREL",
     "onlplab/alephbert-base",
     "dicta-il/dictabert",
+    "imvladikon/alephbertgimmel-base-512",
+    "dicta-il/MsBERT",
     "yonatanlou/BEREL-finetuned-DSS-maskedLM",
     "yonatanlou/alephbert-base-finetuned-DSS-maskedLM",
     "yonatanlou/dictabert-finetuned-DSS-maskedLM",
-    "imvladikon/alephbertgimmel-base-512",
+
 ]
